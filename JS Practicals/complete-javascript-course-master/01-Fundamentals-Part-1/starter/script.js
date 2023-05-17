9a12e7157597ddf2a0b74bedcbdefa025e79f9b0
Dare-Olumuyiwa <dareolumuyiwa@gmail.com>
2023-05-17 16:53:12 +0100

    Datatype cont

---
+++ JS Practicals/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+let js = "amazing";
+console.log(40+56+8-83*2);
+console.log("jonas");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+// Boolean
+javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// typeof
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 342);
+console.log(typeof "AdeOri");
+// Dynamnic typing
+javaScriptIsFun ="YES!"
+console.log(typeof javaScriptIsFun);
+
+// Undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+// effect of dynamic typing
+year = 2023
+console.log(year);
+console.log(typeof year);
+
+// Error that exists in typeof operator
+console.log(typeof null) //Its similar to undefined but typeof return it as object. Hence its error or bugs
+
