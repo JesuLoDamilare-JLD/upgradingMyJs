7db7160a3dcefd106b4ec811875b238fe3b4b7c2
DARE OLUMUYIWA <124059260+JesuLoDamilare-JLD@users.noreply.github.com>
2023-08-09 21:37:26 +0100

    Tutoring Okechukwu

---
+++ JS Practicals/complete-javascript-course-master/oke.js
@@ -0,0 +1,99 @@
+/* alert("Hello world")
+console.log("Hello world") */
+
+//values and variable
+/* console.log("Okechukwu");
+console.log(50);
+
+let firstName = "Umazurike";
+let lastName = "Okechukwu";
+let fullName = firstName + lastName;
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName); */
+
+
+//Data Type
+/* console.log(typeof 23);
+console.log(typeof "Okechukwu");
+
+let js = true;
+console.log(`Is Javascript fun and exciting?`);
+alert(js);
+
+console.log(typeof js); */
+
+let age = 40;
+age = 41;
+console.log(age)
+
+let $function = "okechukwu"
+console.log($function);
+
+
+//Basic Operators
+//Math operators  (+)
+/* x = 3;
+y = 7;
+console.log(x + y);
+
+let firstName = ('Okechukwu');
+let lastName = ('Umezurike')
+let fullName = (firstName + " " + lastName)
+console.log(fullName);
+
+let num = 3;
+let stg = "5"
+let strg = 8
+console.log(num + stg);
+console.log(stg + strg); */
+
+
+
+//camel stype of variable name - myNameIsOkechukwuUmezurike
+
+
+
+//mulitpication
+/* x = 50;
+y = 32;
+z = 3;
+console.log(x * y);
+console.log(x / y);
+console.log(y**z); //exponential
+console.log(y%z); //modulus */
+
+
+//Assignment Operator
+/* z = 60;
+z += 10 // z= z+10 
+console.log(z) // z = 60+10
+
+y = 8;
+y *= 2;
+console.log(y)
+
+
+ //incremental operator
+ x = 9;
+ x++  // x= x+1
+console.log(x); // x= 9+1
+
+//decremental operator
+b = 8
+b-- // b= b-1
+console.log(b); //b = 8-1 */
+
+//comparison operators <,>, <=, >=, ==, ===, !=, !== (they obey to boolean types)
+okeAge = 50;
+joyAge = 40;
+console.log(okeAge < joyAge); //is Oke's age less than Joy's Age?
+
+
+
+
+
+
+
+
